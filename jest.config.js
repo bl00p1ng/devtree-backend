2303e586f3652337ade8238e08cbe2dbfdde9dd5
bl00p1ng <blooping@protonmail.com>
2025-05-27 07:37:05 -0500

    Configuración de jest y TS actualizada para corregir errores

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,15 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+    testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     testTimeout: 10000,
+    setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
     collectCoverage: true,
     collectCoverageFrom: [
         'src/**/*.{ts,tsx}',
         '!src/index.ts',
+        '!src/__tests__/**',
     ],
     coverageDirectory: 'coverage',
     verbose: true,
